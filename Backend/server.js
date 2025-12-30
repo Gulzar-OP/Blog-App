349a7f4018565f41b336618b071be865c645ecb6
Gulzar Hussain <gulzarhu88@gmail.com>
2025-12-30 11:47:37 +0530

    fix

--- Backend/server.js
+++ Backend/server.js
@@ -32,10 +32,11 @@ app.use(limiter);
 
 // ================= CORS =================
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+  origin: [
+    "https://blog-app-giqg.vercel.app",   // frontend URL
+    "http://localhost:5173"
+  ],
+  credentials: true
 }));
 
 // ================= BODY PARSERS =================
